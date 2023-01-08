9f0f321eebfbc04fe610d32b2f8dac955b77b705
Andrij <Amdrij@DESKTOP-CFK8V6F>
2023-01-08 15:47:11 +0200

    Добавьте файлы проекта.

---
+++ Padding.h
@@ -0,0 +1,67 @@
+﻿#ifndef TESTTASK_PADDING_H
+#define TESTTASK_PADDING_H
+
+/** Error code */
+#define RET_OK                return 0  // Successful 
+#define WRONG_PARAMETER_ERROR return 1  // Invalid input parameter
+#define RUNTIME_ERROR         return 2  // Others errors
+
+#include "some.h"
+
+void dataInit(Data* data, size_t size, void* databuf) {
+    data->len = size;
+    data->buf = (uint8_t*)malloc(sizeof(uint8_t) * data->len);
+    if (data->buf == NULL) exit(-1); // 
+    memcpy(data->buf, databuf, data->len);
+}
+
+int kp_pad(Data* data, size_t k) {
+    // input parameter check
+    if (data->len == NULL || data->len <= 0 || data->buf == NULL || (k > 255 && k < 1)) WRONG_PARAMETER_ERROR;
+
+    const size_t kx = k - (data->len % k);                                          // padded count 
+    data->buf = (uint8_t*)realloc(data->buf, (data->len + kx) * sizeof(uint8_t));   // realloc size = data.len + kx
+    if (data->buf == NULL) RUNTIME_ERROR;                                           // if realloc failed
+
+    for (int i = data->len; i < data->len + kx; i++) data->buf[i] = (uint8_t)kx;    // padded cycle
+    data->len += kx; 
+    RET_OK;
+}
+
+int kp_unpad(Data* data, size_t k) {
+    // input parameter check
+    if (data->len == NULL || data->len <= 0 || data->buf == NULL || (k > 255 && k < 1)) WRONG_PARAMETER_ERROR;
+
+    const size_t kx = data->buf[data->len - 1];                                     // padded count 
+    data->buf = (uint8_t*)realloc(data->buf, (data->len - kx) * sizeof(uint8_t));   // realloc size = data.len - kx
+    if (data->buf == NULL) RUNTIME_ERROR;                                           // if realloc failed
+
+    data->len -= kx;
+    RET_OK;
+
+    /*
+    * there are no cycles and mem rewrites here, as far as I understand, realloc frees old memory
+    */
+}
+
+int kp_pad_alloc(Data* data, size_t k, Data* padded) {
+    /*
+    * just allocs new data(padded) memcpy data -> padded and kp_pad( padded )
+    */
+
+    if (data->len == 0 || data->buf == NULL || (k > 255 && k < 1)) WRONG_PARAMETER_ERROR;
+    dataInit(padded, data->len, data->buf);
+    return kp_pad(padded, k);
+}
+
+int kp_unpad_alloc(Data* data, size_t k, Data* unpadded) {
+    /*
+    * just allocs new data(unpadded) memcpy data -> unpadded and kp_pad( unpadded )
+    */
+
+    if (data->len == 0 || data->buf == NULL || (k > 255 && k < 1)) WRONG_PARAMETER_ERROR;
+    dataInit(unpadded, data->len, data->buf);
+    return kp_unpad(unpadded, k);
+}
+
+#endif//TESTTASK_PADDING_H
---
+++ RFC-5652_Padding.c
@@ -0,0 +1,108 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "Padding.h"
+
+int fullRandomTest();
+int reedbleRandomTest();
+
+int main(){
+
+    Data data1; const int k1 = 0x10;
+    dataInit(&data1, 16, (uint8_t[]) { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f });
+    Data data2; const int k2 = 0x0f;
+    dataInit(&data2, 07, (uint8_t[]) { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06 });
+    Data data3; const int k3 = 0x10;
+    dataInit(&data3, 32, (uint8_t[]) {
+        0x10, 0x10, 0x10, 0xa10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10
+            , 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10
+    });
+
+    printf("k = %i\n", k1);
+    printData(data1);
+    kp_pad(&data1, k1);
+    colorPrintData(data1, 16, Red);
+
+    printf("\nk = %i\n", k2);
+    printData(data2);
+    kp_pad(&data2, k2);
+    colorPrintData(data2, 07, Green);
+
+    printf("\nk = %i\n", k3);
+    printData(data3);
+    kp_unpad(&data3, k3);
+    printData(data3);
+
+    Data data;
+    dataInit(&data, 8, (uint8_t[]) { 0xfd, 0x3d, 0xa1, 0x00, 0x20, 0xcc, 0xe7, 0xff });
+    dataExamplePadUnpad(data,0x0a, Yellow);
+
+    
+    l1:printf("\nStart random test?(Y/N  (R -reedble rundom test)):");
+    char ch = getchar();
+
+    switch (ch){
+    case 'N': {
+        printf("exit");
+        exit(0);
+        break;
+    }
+    case 'Y': {
+        printf("Start, will stop if get error or CTR+C :) \n");
+        srand(NULL);
+
+        while (!fullRandomTest());
+
+        break;
+    }
+    case 'R': {
+        printf("Start, will stop if get error or CTR+C :) \n");
+        srand(NULL);
+        while (!reedbleRandomTest());
+
+        break;
+    }
+    default: {
+        printf("\twrong input\n");
+        goto l1;
+        break;
+    }
+    }
+
+}
+
+char* randomColorT_T() {
+    switch (rand() % 7) {
+    case 0: return Red;
+    case 1: return Green;
+    case 2: return Yellow;
+    case 3: return Blue;
+    case 4: return Purple;
+    case 5: return Cyan;
+    case 6: return White;
+    }
+}
+
+uint8_t* randomData(size_t size) {
+    uint8_t* buf = calloc(size, sizeof(uint8_t));
+
+    for (int i = 0; i < size; i++) buf[i] = rand() % 0xff;
+    return buf;
+}
+
+int fullRandomTest() {
+    Data data;
+    size_t size = (rand() % 0xfe) + 0x01;
+    size_t k = (rand() % 0xfe) + 0x01;
+
+    dataInit(&data, size, randomData(size));
+    return dataExamplePadUnpad(data, k, randomColorT_T());
+}
+
+int reedbleRandomTest() {
+    Data data;
+    size_t size = (rand() % 0x19) + 0x05;
+    size_t k = (rand() % 0x1d) + 0x02;
+
+    dataInit(&data, size, randomData(size));
+    return dataExamplePadUnpad(data, k, randomColorT_T());
+}
+
